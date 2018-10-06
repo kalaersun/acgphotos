cc9033bf7e90c018d575713a9c2a9484d39799d4
changge <changge@pinduoduo.com>
2018-10-06 14:11:50 +0800

    111

---
+++ src/componment/Login/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Button,Input} from 'antd';
+import './index.scss'
+export default class Login extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            username:'',
+            password:''
+        }
+    }
+    render(){
+        const {username,password}=this.state
+        return(
+            <div className="login-module">
+                <div className="login-module-section">
+                    <div className="login-module-section-title">
+                        用户名：
+                    </div>
+                    <div className="login-module-section-input">
+                        <Input placeholder="用户名" value={username}/>
+                    </div>
+                </div>
+                <div className="login-module-section">
+                    <div className="login-module-section-title">
+                        密码：
+                    </div>
+                    <div className="login-module-section-input">
+                    <Input placeholder="密码"  value={password}/>
+                    </div>
+                </div>
+                <div className="login-module-section">
+                <Button>登录</Button>
+                <Button>注册</Button>
+                </div>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Login from './componment/Login'
+class App extends React.Component{
+    render(){
+        return(
+                <Login/>
+        )
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(<App/>, document.getElementById('root'));
 serviceWorker.unregister();
